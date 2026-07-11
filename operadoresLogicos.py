0e491c2455f5deca3e1785787a9f5bbe147ff35b
Cauan Pinho <Jobscauanpinho@gmail.com>
2026-07-11 12:23:50 -0300

    Operadores Logicos

---
+++ operadoresLogicos.py
@@ -0,0 +1,13 @@
+num1 = 10
+num2 = 10
+
+print(num1 != num2)
+print(num1 == num2)
+print(num1 >= num2)
+print(num1 <= num2)
+
+
+idade = int(input("digite sua idade"))
+verificador = idade >= 18
+
+print(f"voce é maior de idade? {verificador}")
