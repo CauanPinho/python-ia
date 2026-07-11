04e06bef413d3c6361c7465523ab6df4eda242e1
Cauan Pinho <Jobscauanpinho@gmail.com>
2026-07-11 12:50:50 -0300

    Ifelse

---
+++ ifelse.py
@@ -0,0 +1,6 @@
+numero = int(input("Digite um numero: "))
+
+if numero == 10:
+    print("Vc acertou o numero")
+else:
+    print("Vc errou o numero")
---
+++ notaescolar.py
@@ -0,0 +1,9 @@
+notaAluno=float(input("Digite a nota do aluno: "))
+
+
+if notaAluno >=7:
+    print("Aluno aprovado")
+elif notaAluno >=5:
+    print("Aluno esta em recuperação")
+else:
+    print("Aluno reprovado")
