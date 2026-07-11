9dc8a75e5cc14f5601833d6ccd3b1f1cc9d720c4
Cauan Pinho <Jobscauanpinho@gmail.com>
2026-07-11 12:19:09 -0300

    OperadoresRelacionais

---
+++ operadoresRelacioanais.py
@@ -0,0 +1,6 @@
+quantidade=(int(input("Digite a quantidade em ml do produto")))
+
+
+pordia=int(input("Qual a quantidade em ml voce pretende usar por dia?"))
+
+print(f"o produto durara: {quantidade/pordia:.0f} dias")
