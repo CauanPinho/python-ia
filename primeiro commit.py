5d096b2696633d3a21760718dccf73a2472ff1fd
Cauan Pinho <Jobscauanpinho@gmail.com>
2026-07-11 12:04:04 -0300

    Operadores lógicos e Matemáticos

---
+++ primeiro commit.py
@@ -0,0 +1,8 @@
+produto = input("Digite o nome do produto: ")
+
+
+preco = float(input("Digite o preço do produto"))
+
+desconto = 0.5
+
+print(f"O novo preço do {produto} com desconto é: {preco * desconto}")
