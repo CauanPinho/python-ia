74fe06b0bab329dfb80dc6dc84f5c225fe3a37b9
Cauan Pinho <Jobscauanpinho@gmail.com>
2026-07-13 11:11:05 -0300

    POO

--- POO/carros.py
+++ POO/carros.py
@@ -3,12 +3,13 @@ class Carro:
         self.cor=cor
         self.ano=ano
         self.ligado=True
+        self.seta= None
     
     def informacoes(self):
         print(f" A cor do carro e {self.cor}")  
         print(f"O ano do carro e {self.ano}")
     
-    def ligar_sekf(self):
+    def ligar_self(self):
         if not self.ligado:
             self.ligado=True
             print("O carro ja está ligado")
@@ -21,10 +22,12 @@ class Carro:
             print("O carro está desligado")
         else:
             print("O carro já está desligado")
-
+    def ligarSeta(self,direcao):
+        self.seta= direcao
+        print(f"A seta do carro está ligada para a {self.seta}")
 
     
-carro1 = Carro('Azul', 2020)
-
+carro1 = Carro('Azul', 2020,True)
+carro1.ligarSeta('direita')
 carro1.informacoes()
-carro1.ligar()
+carro1.ligar_self()
